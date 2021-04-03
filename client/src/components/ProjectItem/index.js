aa64d066f7765ecf35b6f221fe7ee749c2b4cf81
Taani Maama <taani1maama@live.com>
2021-04-03 13:48:48 -0700

    Updated the ProjectItems to have descriptions.

--- client/src/components/ProjectItem/index.js
+++ client/src/components/ProjectItem/index.js
@@ -1,16 +1,17 @@
 import React from "react";
 
-const ProjectItem = ({ projNum, title, image, github, liveUrl }) => {
+const ProjectItem = ({ projNum, title, desc, image, github, liveUrl }) => {
   return (
     <div className={`proj-${projNum}`}>
-      <div className='img__container proj__img'>
-        <img src={image} alt={title} loading='lazy' />
-        <div className='overlay'>
-          <p>{title}</p>
-          <a href={github} target='_blank' rel='noreferrer'>
+      <div className="img__container proj__img">
+        <img src={image} alt={title} loading="lazy" />
+        <div className="overlay">
+          <p id="proj-title">{title}</p>
+          <p id="proj-desc">{desc}</p>
+          <a href={github} target="_blank" rel="noreferrer">
             GitHub Repo
           </a>
-          <a href={liveUrl} target='_blank' rel='noreferrer'>
+          <a href={liveUrl} target="_blank" rel="noreferrer">
             Live URL
           </a>
         </div>
--- client/src/pages/Projects/index.js
+++ client/src/pages/Projects/index.js
@@ -4,7 +4,7 @@ import Main from "../../components/Main";
 import ProjectItem from "../../components/ProjectItem";
 import trailChasers from "../../images/tinified/trailchasers.png";
 import budgetTracker from "../../images/tinified/budgettracker.png";
-import eatDaBurger from "../../images/tinified/eat-da-burger.png";
+import dono from "../../images/tinified/dono.png";
 import oneWorldAcademy from "../../images/tinified/oneworldacademy.png";
 import teamBase from "../../images/tinified/teambase.png";
 import fitnessTracker from "../../images/tinified/fitness-tracker.png";
@@ -14,25 +14,42 @@ const Projects = () => {
   return (
     <div>
       <Main>
-        <GridContainer className='grid__proj'>
-          <GridItem id='proj-header'>
+        <GridContainer className="grid__proj">
+          <GridItem id="proj-header">
             <h1>Projects</h1>
           </GridItem>
-          <GridItem id='projects'>
-            <p className='note'>*Hover over images for links and details*</p>
+          <GridItem id="projects">
+            <p className="note">*Hover over images for links and details*</p>
 
-            <div className='projects__container'>
+            <div className="projects__container">
               <ProjectItem
                 projNum={1}
+                title={"Dono: Charity Trend"}
+                desc={
+                  "Dono is a social media decluttered of the normal social interactions and focuses on charities and giving back. Made with MERN stack, Socket.io, Material UI, and Cloudinary."
+                }
+                image={dono}
+                github={"https://github.com/TaaniBravo/13-Eat-Da-Burger"}
+                liveUrl={"https://eat-da-burger-or-else.herokuapp.com/"}
+              />
+              <br />
+              <ProjectItem
+                projNum={2}
                 title={"One World Academy LMS"}
+                desc={
+                  "A custom learning management system that allows you to teach your way. Built with Express, Sequelize, Bootstrap, and Handlebars."
+                }
                 image={oneWorldAcademy}
                 github={"https://github.com/TaaniBravo/One-World-Academy"}
                 liveUrl={"https://oneworldacademy.herokuapp.com/"}
               />
               <br />
               <ProjectItem
-                projNum={2}
+                projNum={3}
                 title={"Trail Chasers Hiking App"}
+                desc={
+                  "A fun hike finding app that allows you to search for hikes in any location and then finds a route for you to picked hike. Made with HTML, Bulma, OpenWeatherMap, Google Maps, and Hiking Project API"
+                }
                 image={trailChasers}
                 github={"https://github.com/TaaniBravo/Trail-Chaser-Hiking-App"}
                 liveUrl={
@@ -41,16 +58,11 @@ const Projects = () => {
               />
               <br />
               <ProjectItem
-                projNum={3}
-                title={"Eat Da Burger"}
-                image={eatDaBurger}
-                github={"https://github.com/TaaniBravo/13-Eat-Da-Burger"}
-                liveUrl={"https://eat-da-burger-or-else.herokuapp.com/"}
-              />
-              <br />
-              <ProjectItem
                 projNum={4}
                 title={"TeamBase: Employee Directory"}
+                desc={
+                  "A fun and simple employee directory. Made with React and Random Employee API"
+                }
                 image={teamBase}
                 github={"https://github.com/TaaniBravo/19-Employee-Directory"}
                 liveUrl={"https://teambase-directory.herokuapp.com/"}
@@ -59,6 +71,9 @@ const Projects = () => {
               <ProjectItem
                 projNum={5}
                 title={"IndexDB Budget Tracker"}
+                desc={
+                  "A online and offline budget tracker that allows the user to always update their budget. Focus was the practice of using the IndexDb on client's browser."
+                }
                 image={budgetTracker}
                 github={
                   "https://github.com/TaaniBravo/Online-Offline-Budget-Trackers"
@@ -69,41 +84,44 @@ const Projects = () => {
               <ProjectItem
                 projNum={6}
                 title={"Daily Fitness Tracker"}
+                desc={
+                  "A simple fitness tracker that allows for the user to make workouts and save them and track their weekly progress."
+                }
                 image={fitnessTracker}
                 github={"https://github.com/TaaniBravo/17-Workout-Tracker"}
                 liveUrl={"https://daily-fitness-tracker.herokuapp.com/"}
               />
             </div>
           </GridItem>
-          <GridItem id='skillset'>
-            <div className='skillset__container'>
-              <h2 className='lang__header'>Current Languages</h2>
+          <GridItem id="skillset">
+            <div className="skillset__container">
+              <h2 className="lang__header">Current Languages</h2>
               <br />
-              <div className='sub-container'>
-                <ul className='language__icons'>
+              <div className="sub-container">
+                <ul className="language__icons">
                   <li>
-                    <i className='fab fa-html5'></i> HTML
+                    <i className="fab fa-html5"></i> HTML
                   </li>
                   <li>
-                    <i className='fab fa-js-square'></i> JS
+                    <i className="fab fa-js-square"></i> JS
                   </li>
                   <li>
-                    <i className='fab fa-css3-alt'></i> CSS
+                    <i className="fab fa-css3-alt"></i> CSS
                   </li>
                   <li>
-                    <i className='fab fa-sass'></i> SCSS
+                    <i className="fab fa-sass"></i> SCSS
                   </li>
                   <li>
-                    <i className='fas fa-database'></i> SQL and NoSQL
+                    <i className="fas fa-database"></i> SQL and NoSQL
                   </li>
                   <li>
-                    <i className='fab fa-react'></i> ReactJS
+                    <i className="fab fa-react"></i> ReactJS
                   </li>
                 </ul>
               </div>
               <br />
               <h4>Currently working towards...</h4>
-              <div className='sub-container'>
+              <div className="sub-container">
                 <ul>
                   <li>Advanced JS Algo thinking.</li>
                   <li>Deep knowledge base of CSS.</li>
