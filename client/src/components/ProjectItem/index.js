691ad0961577441fdd84a127c8f0e211fc35fb0d
Taani Maama <taani1maama@live.com>
2021-07-10 09:09:22 -0700

    Added a class that I can style for all project items.

--- client/src/components/ProjectItem/index.js
+++ client/src/components/ProjectItem/index.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import { Button, Modal } from 'react-bootstrap';
-import './style.scss';
+import React, { useEffect, useState } from "react";
+import { Button, Modal } from "react-bootstrap";
+import "./style.scss";
 
 const ProjectItem = ({ projNum, title, desc, image, github, liveUrl }) => {
   const [width, setWidth] = useState(window.innerWidth);
@@ -14,27 +14,27 @@ const ProjectItem = ({ projNum, title, desc, image, github, liveUrl }) => {
   useEffect(() => {
     const handleWindowResize = () => setWidth(window.innerWidth);
 
-    window.addEventListener('resize', handleWindowResize);
+    window.addEventListener("resize", handleWindowResize);
     return () => {
-      window.removeEventListener('resize', handleWindowResize);
+      window.removeEventListener("resize", handleWindowResize);
     };
   }, []);
 
   return width > breakpoint ? (
-    <div className={`proj-${projNum}`}>
-      <div className="img__container proj__img">
-        <img src={image} alt={title} loading="lazy" />
-        <div className="overlay">
-          <p id="proj-title">{title}</p>
-          <p id="proj-desc">{desc}</p>
-          <div className="btn-container">
-            <Button id="githubBtn">
-              <a href={github} target="_blank" rel="noreferrer">
+    <div className={`proj-${projNum} proj-item`}>
+      <div className='img__container proj__img'>
+        <img src={image} alt={title} loading='lazy' />
+        <div className='overlay'>
+          <p id='proj-title'>{title}</p>
+          <p id='proj-desc'>{desc}</p>
+          <div className='btn-container'>
+            <Button id='githubBtn'>
+              <a href={github} target='_blank' rel='noreferrer'>
                 GitHub Repo
               </a>
             </Button>
-            <Button id="liveUrlBtn">
-              <a href={liveUrl} target="_blank" rel="noreferrer">
+            <Button id='liveUrlBtn'>
+              <a href={liveUrl} target='_blank' rel='noreferrer'>
                 Live URL
               </a>
             </Button>
@@ -50,21 +50,21 @@ const ProjectItem = ({ projNum, title, desc, image, github, liveUrl }) => {
         </Modal.Header>
         <Modal.Body>{desc}</Modal.Body>
         <Modal.Footer>
-          <Button id="githubBtn" variant="warning" onClick={handleClose}>
-            <a href={github} target="_blank" rel="noreferrer">
+          <Button id='githubBtn' variant='warning' onClick={handleClose}>
+            <a href={github} target='_blank' rel='noreferrer'>
               GitHub Repo
             </a>
           </Button>
-          <Button id="liveUrlBtn" variant="primary" onClick={handleClose}>
-            <a href={liveUrl} target="_blank" rel="noreferrer">
+          <Button id='liveUrlBtn' variant='primary' onClick={handleClose}>
+            <a href={liveUrl} target='_blank' rel='noreferrer'>
               Live URL
             </a>
           </Button>
         </Modal.Footer>
       </Modal>
       <div className={`proj-${projNum}`}>
-        <div className="img__container proj__img">
-          <img src={image} alt={title} loading="lazy" onClick={handleShow} />
+        <div className='img__container proj__img'>
+          <img src={image} alt={title} loading='lazy' onClick={handleShow} />
         </div>
       </div>
     </>
